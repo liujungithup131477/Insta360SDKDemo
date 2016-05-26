71d572ef29b14e8cec047dac3d39b86982bdaa5a
junliu <junliu@pptv.com>
2016-05-26 16:00:24 +0800

    ARVPlayerItem.h 文件的查看注释

--- Insta360SDKDemo/Lib/ARVPlayerController.framework/Headers/ARVPlayerItem.h
+++ Insta360SDKDemo/Lib/ARVPlayerController.framework/Headers/ARVPlayerItem.h
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Insta360. All rights reserved.
 //
 
+// MARK: - 播放单个视频文件类
+
 #import <Foundation/Foundation.h>
 #import <INSMediaSDK/INSMediaSDK.h>
 
@@ -19,11 +21,16 @@ typedef enum:NSInteger
 }ARVItemType;
 
 @interface ARVPlayerItem : NSObject
-
+/**
+ *  初始化控制器，创建播放单个视频文件的播放控制器
+ */
 - (instancetype)initWithURL:(NSURL *)url offset:(NSString *)offset type:(ARVItemType)itemType;
 
+/** 播放文件源的数据类型*/
 @property (nonatomic, assign, readonly) ARVItemType itemType;
+/** 播放文件源的路径*/
 @property (nonatomic, strong, readonly) NSURL *itemURL;
+/** 视频文件播放的偏移量*/
 @property (nonatomic, copy, readonly) NSString *itemOffset;
 
 @end
