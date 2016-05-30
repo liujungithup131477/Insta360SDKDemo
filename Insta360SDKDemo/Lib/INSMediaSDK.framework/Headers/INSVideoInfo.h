f027ce2d3768bfbf865bcb524b87bff3dba8c2dd
junliu <junliu@pptv.com>
2016-05-30 15:40:30 +0800

    INSVideoInfo.h 视频的基本信息

--- Insta360SDKDemo/Lib/INSMediaSDK.framework/Headers/INSVideoInfo.h
+++ Insta360SDKDemo/Lib/INSMediaSDK.framework/Headers/INSVideoInfo.h
@@ -13,11 +13,29 @@
 
 -(instancetype)initWithFrameRate:(double)framerate duration:(double)duration width:(int)width height:(int)height;
 
+/**
+ *   帧速率
+ */
 @property (nonatomic, readonly) double frameRate;
+/**
+ *  视频持续的时间
+ */
 @property (nonatomic, readonly) double duration;
+/**
+ *  宽度
+ */
 @property (nonatomic, readonly) int width;
+/**
+ *  高度
+ */
 @property (nonatomic, readonly) int height;
+/**
+ *  偏移量
+ */
 @property (nonatomic, strong, readwrite) NSString *offset;
+/**
+ *  截屏图片
+ */
 @property (nonatomic, strong, readwrite) UIImage *screenShotImage;
 
 @end
